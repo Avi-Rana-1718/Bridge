058fbc84eae95723ecf14b01953c5a95459e54c5
Avi Rana <itzavi1718@gmail.com>
2023-01-13 22:47:58 +0530

    IOS Detection

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ document.getElementById("device").innerHTML = `Posted from ${res.platform} `;
 
 let platform;
 function post() {
-  if (navigator.userAgent.match(/Android/i)==true)
+  if (navigator.userAgent.match(/Android/i)==true) {
   platform="Android";
-  else 
+  } else if (navigator.userAgent.match(/iPhone/i)==true) {
+    platform="iPhone";
+  } else {
   platform="Windows (PC)";
+  }
     firebase.database().ref('data/ref/avi').set({
         data: document.getElementById('clipboard').value,
         platform: platform
